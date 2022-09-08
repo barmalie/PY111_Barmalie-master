9cc37c7bbeedbd09625fa61cefe47b4a81f852d0
russos <ro0ne@mail.ru>
2022-09-08 19:14:20 +0300

    after writing selsky

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -7,7 +7,8 @@ from typing import Any
 
 class Stack:
     def __init__(self):
-        self.stack = [] # todo для стека можно использовать python list
+        self.my_stack = [] # todo для стека можно использовать python list
+        self.reversed_index = 0
 
     def push(self, elem: Any) -> None:
         """
@@ -16,10 +17,10 @@ class Stack:
         :param elem: element to be pushed
         :return: Nothing
         """
-        self.elem = elem
-        initial_elem = stack.push(elem)
+        self.my_stack.append(elem)
+
         print(elem)
-        return initial_elem
+        return None
 
     def pop(self) -> Any:
         """
@@ -27,11 +28,10 @@ class Stack:
 
         :return: popped element
         """
-        self.elem = elem.pop
-        initial_elem = stack.pop(elem)
-        print(elem[])
 
-        return None
+        if not self.my_stack:
+            return None
+        return self.my_stack.pop()
 
     def peek(self, ind: int = 0) -> Any:
         """
@@ -39,11 +39,16 @@ class Stack:
 
         :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
         :return: peeked element or None if no element in this place
-        """
-        self.ind = elem.call[ind]
 
-        print(ind)
-        return None
+
+        """
+        self.reversed_index = -ind-1
+        try:
+            stack[ind]
+        except IndexError:
+            return None
+        return self.my_stack[self.reversed_index]
+
 
     def clear(self) -> None:
         """
@@ -51,4 +56,5 @@ class Stack:
 
         :return: None
         """
+        self.my_stack.clear()
         return None
