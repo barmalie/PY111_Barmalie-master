e691f8ab4ba8daec6bc42f1c4dbb6052864d6b6b
russos <ro0ne@mail.ru>
2022-09-10 13:10:15 +0300

    сыровато пока

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,7 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+
 
 
 def fib_iterative(n: int) -> int:
@@ -16,5 +15,25 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if < 0:
+        raise ValueError
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    a = 0
+    b = 1
+    for i in range(1,n):
+        sum_ = a + b
+        a = b
+        b =sum_
+        return b
+
+# from functools import reduce
+# ## Данные
+# n = 10
+# ## Однострочник
+# fibs = reduce(lambda x, _: x + [x[-2] + x[-1]], [0] * (n - 2), [0, 1])
+# ## Результат
+# print(fibs)
+
