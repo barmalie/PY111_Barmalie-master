e8ebe60e0b81cb9c58cc8a3c3d689d8a03aa48b1
russos <ro0ne@mail.ru>
2022-09-10 12:07:12 +0300

    Второй однострочный вариант

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -26,6 +26,12 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
 l = [3, 6, 14, 16, 33, 55, 56, 89]
 x = 56
 print(binary_search(x, l))
-# 6 (индекс найденного элемента
-#print(elem, arr)
-    #return None
+
+
+
+bs = lambda l, x, lo, hi: -1 if lo>hi else \
+(lo+hi)//2 if l[(lo+hi)//2] == x else \
+bs(l, x, lo, (lo+hi)//2-1) if l[(lo+hi)//2] > x else \
+bs(l, x, (lo+hi)//2+1, hi)
+
+print(bs(l, x, 0, len(l)-1))
