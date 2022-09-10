c74e585ac3ba6a75de41ebdb478739180f75bdf6
russos <ro0ne@mail.ru>
2022-09-10 12:09:37 +0300

    Однострочник закоммитил

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -2,13 +2,13 @@ from typing import Sequence, Optional
 
 
 def binary_search(elem: int, arr: Sequence) -> Optional[int]:
-    # """
-    # Performs binary search of given element inside of array
-    #
-    # :param elem: element to be found
-    # :param arr: array where element is to be found
-    # :return: Index of element if it's presented in the arr, None otherwise
-    # """
+    """
+    Performs binary search of given element inside of array
+
+    :param elem: element to be found
+    :param arr: array where element is to be found
+    :return: Index of element if it's presented in the arr, None otherwise
+    """
 
 
     low = 0
@@ -29,9 +29,9 @@ print(binary_search(x, l))
 
 
 
-bs = lambda l, x, lo, hi: -1 if lo>hi else \
-(lo+hi)//2 if l[(lo+hi)//2] == x else \
-bs(l, x, lo, (lo+hi)//2-1) if l[(lo+hi)//2] > x else \
-bs(l, x, (lo+hi)//2+1, hi)
-
-print(bs(l, x, 0, len(l)-1))
+# bs = lambda l, x, lo, hi: -1 if lo>hi else \
+# (lo+hi)//2 if l[(lo+hi)//2] == x else \
+# bs(l, x, lo, (lo+hi)//2-1) if l[(lo+hi)//2] > x else \
+# bs(l, x, (lo+hi)//2+1, hi)
+#
+# print(bs(l, x, 0, len(l)-1))
