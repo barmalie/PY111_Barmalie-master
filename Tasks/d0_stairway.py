5e574edad8a68e5830eb7de5d2d1e8076624d0e4
russos <ro0ne@mail.ru>
2022-09-15 20:20:42 +0300

    ПОка не рабобтает

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -9,21 +9,38 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :return: minimal cost of getting to the top
     """
     print(stairway)
-    return direct_stairway_path(stairway)
-def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
-    #stair_way цена одной ступеньки
-    count_stairs = len(stairway) # количество
-    total_cost: list[int] = [0] * count_stairs # стоимость дойти до ступеньки
+    return reverse_stairway_path(stairway)
+    #прямой метод
+# def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+#     #stair_way цена одной ступеньки
+#     count_stairs = len(stairway) # количество
+#     total_cost: list[int] = [0] * count_stairs # стоимость дойти до ступеньки
+#
+#     # изначальные условия
+#     total_cost[0] = stairway[0] # для первой ступени
+#     total_cost[1] = stairway[1] # для второй ступени
+#     for i in range(2, count_stairs):
+#         total_cost[i] = stairway[i] + min(total_cost[i-1], total_cost[i-2])
+#     return total_cost[-1]
+#
+#     # #прямой метод обхода: i стоимость =i цена + min((i-1)стоимость,(i-2)стоимость)
+#
+def reverse_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    # stair_way цена одной ступеньки
+    count_stairs = len(stairway)  # количество
+    total_cost: list[int] = [0] * count_stairs  # стоимость дойти до ступеньки
 
     # изначальные условия
-    total_cost[0] = stairway[0] # для первой ступени
-    total_cost[1] = stairway[1] # для второй ступени
-    for i in range(2,count_stairs):
-        total_cost[i] = stairway[i]+ min(total_cost[i-1], total_cost[i-2])
-    return total_cost[-1]
+    total_cost[0] = stairway[0]  # для первой ступени
+    total_cost[1] = stairway[1]  # для второй ступени
+    for i in range(2, count_stairs):
+        if stairway[0] == 1:
+            return total_cost[0]
 
-    # #прямой метод обхода: i стоимость =i цена + min((i-1)стоимость,(i-2)стоимость)
-    # cost = list[int] + min(count_stairs - total_cost[1], count_stairs-total_cost[2])
-    # print(cost)
-    # return 0
+        if stairway[1] == 2:
+            return total_cost[1]
+
+        current_stair = count_stairs  # стоимость текущей ступени
+
+        return current_stair + min(reverse_stairway_path(i - 1), reverse_stairway_path(i - 2))
 
