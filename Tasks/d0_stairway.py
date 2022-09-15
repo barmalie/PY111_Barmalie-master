2007f772bb328ae80a6c26c183619f883020f116
russos <ro0ne@mail.ru>
2022-09-15 20:35:27 +0300

    Пока не работает 2

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -24,23 +24,21 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
 #     return total_cost[-1]
 #
 #     # #прямой метод обхода: i стоимость =i цена + min((i-1)стоимость,(i-2)стоимость)
-#
+
+
+# см лекцию
 def reverse_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     # stair_way цена одной ступеньки
-    count_stairs = len(stairway)  # количество
-    total_cost: list[int] = [0] * count_stairs  # стоимость дойти до ступеньки
-
+    count_stairs = len(stairway)  # количество ok
+    total_cost: list[int] = [float("inf")] * count_stairs  # стоимость дойти до ступеньки ok
     # изначальные условия
-    total_cost[0] = stairway[0]  # для первой ступени
-    total_cost[1] = stairway[1]  # для второй ступени
-    for i in range(2, count_stairs):
-        if stairway[0] == 1:
-            return total_cost[0]
+    total_cost[0] = stairway[0]  # для первой ступени ok
+    total_cost[1] = stairway[1]  # для второй ступени ok
+    for i in range(0, count_stairs):
+        if i + 1 < count_stairs:
+            total_cost[i + 1] = min(total_cost[i]+stairway[i+1], total_cost[i+1])
+        if i + 2 < count_stairs:
+            total_cost[i + 2] = min(total_cost[i] + stairway[i + 2], total_cost[i + 2])
+        return total_cost[-1]
 
-        if stairway[1] == 2:
-            return total_cost[1]
-
-        current_stair = count_stairs  # стоимость текущей ступени
-
-        return current_stair + min(reverse_stairway_path(i - 1), reverse_stairway_path(i - 2))
 
