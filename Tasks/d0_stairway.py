e22a76d1b693454398ee3e0d160cdfcbc79f84fd
russos <ro0ne@mail.ru>
2022-09-15 19:25:51 +0300

    Работает

--- Tasks/d0_stairway.py
+++ Tasks/d0_stairway.py
@@ -9,4 +9,21 @@ def stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
     :return: minimal cost of getting to the top
     """
     print(stairway)
-    return 0
+    return direct_stairway_path(stairway)
+def direct_stairway_path(stairway: Sequence[Union[float, int]]) -> Union[float, int]:
+    #stair_way цена одной ступеньки
+    count_stairs = len(stairway) # количество
+    total_cost: list[int] = [0] * count_stairs # стоимость дойти до ступеньки
+
+    # изначальные условия
+    total_cost[0] = stairway[0] # для первой ступени
+    total_cost[1] = stairway[1] # для второй ступени
+    for i in range(2,count_stairs):
+        total_cost[i] = stairway[i]+ min(total_cost[i-1], total_cost[i-2])
+    return total_cost[-1]
+
+    # #прямой метод обхода: i стоимость =i цена + min((i-1)стоимость,(i-2)стоимость)
+    # cost = list[int] + min(count_stairs - total_cost[1], count_stairs-total_cost[2])
+    # print(cost)
+    # return 0
+
