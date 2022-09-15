2a0bd0dc374266b2c7a09a0bb407b592164afb5b
russos <ro0ne@mail.ru>
2022-09-15 18:49:10 +0300

    Доработать

--- Tasks/c1_fact.py
+++ Tasks/c1_fact.py
@@ -1,14 +1,20 @@
-def factorial_recursive(n: int) -> int:
-    """
-    Calculate factorial of number n (> 0) in recursive way
-    :param n: int > 0
-    :return: factorial of n
-    """
-    bad_variant = []
-    good_variant = []
-    for i in cost_variant =
-    print(n)
-    return 0
+# def factorial_recursive(n: int) -> int:
+#     """
+#     Calculate factorial of number n (> 0) in recursive way
+#     :param n: int > 0
+#     :return: factorial of n
+#     """
+#     cost_variant = (1,4,3,2,5)
+#     bad_variant = []
+#     good_variant = []
+#     for i in cost_variant():
+#         if sum(bad_variant(i))<sum(good_variant(1)):
+#         #if bad_variant<good_variant:
+#             print("OK")
+#         else:
+#             print("Not OK")
+#     print(n)
+#     return 0
 
 
 def factorial_iterative(n: int) -> int:
