b735fa7b7d6ec2c7ff352bec357cfc7fc4222e4a
russos <ro0ne@mail.ru>
2022-09-23 18:37:34 +0300

    Пока не работает  пишем код

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -6,5 +6,20 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     :param point: desired point for horse
     :return: count of paths from (1, 1) to (point[0], point[1]) (numerating from 0, so (0, 0) - left bottom tile)
     """
-    print(shape, point)
-    return 11
+    rows = shape[0]
+    cols = shape[0]
+    def get_step(i,j):
+        if i == 0 and j == 0: #первый угол.  попали в начальную точку
+            return 1
+        if not 0 <= i < rows: #первый угол
+            return 0
+        if not 0 <= i < cols: # выпали за строки
+            return 0
+        return 1
+
+
+    return
+
+if __name__ == "__main__":
+    assert 2 == calculate_paths()
+    assert 2 == calculate_paths()
