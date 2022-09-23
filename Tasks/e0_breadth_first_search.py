64f43779db988a7f86dcc7b7a9279cb522ef5b30
russos <ro0ne@mail.ru>
2022-09-23 19:27:06 +0300

    и здесь написали написали

--- Tasks/e0_breadth_first_search.py
+++ Tasks/e0_breadth_first_search.py
@@ -1,6 +1,7 @@
 from typing import Hashable, List
 import networkx as nx
-
+from collections import deque
+import matplotlib.pyplot as plt
 
 def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     """
@@ -10,5 +11,28 @@ def bfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node for search
     :return: list of nodes in the visited order
     """
+    path_node = []
+    visited_node = {node: False for node in g.nodes}
+    wait_nodes = deque()
+    wait_nodes.append(start_node)
+    while wait_nodes:
+        current_node = wait_nodes.popleft() # забираем первый элемент start_node
+        path_node.append(current_node)
+        visited_node[current_node] = True # узел сгорел
+
+        for neighbour in g[current_node]:
+            if not visited_node[neighbour]:
+                wait_nodes.append(neighbour) # список подожженных(подожгли соседа)
+                visited_node[neighbour] = True
+
+
     print(g, start_node)
-    return list(g.nodes)
+    return(path_node)
+
+
+def draw_graph(graph):
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_labels(graph, pos)
+
+    for edge in graph.edges:
