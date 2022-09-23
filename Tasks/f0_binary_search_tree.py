0f71b7fba791d3941684a3446f5bea48a2eb8969
russos <ro0ne@mail.ru>
2022-09-23 21:27:39 +0300

    и здесь написали написали

--- Tasks/f0_binary_search_tree.py
+++ Tasks/f0_binary_search_tree.py
@@ -2,16 +2,58 @@
 You can do it either with networkx ('cause tree is a graph)
 or with dicts (smth like {'key': 0, value: 123, 'left': {...}, 'right':{...}})
 """
+node = {
+    "key":
+    "value":
+    "left":
+    "rigth":
+}
 
+root = {
+    "key": 8,
+
+    "left": None {"key": 8,
+                    "value": 3,
+                    "left": None,
+                    "rigth": None,
+                   }
+    "rigth": None,
+}
 from typing import Any, Optional, Tuple
 # import networkx as nx
 
 
 class BinarySearchTree:
+    @staticmethod
+    def _create_node(key:, value:Any, left:Optional[dict],rigth:Optional[dict],):
+        return { "key":key,
+               "value": value,
+                "left": left,
+                "right": right,
+                }
     def __init__(self):
-        ...
-
+        self.root:Optional[dict]=None
     def insert(self, key: int, value: Any) -> None:
+        if self.root is None:
+            self.root = self._create_node(key,value)
+        else:
+            current_node = self.root
+            while True:
+                current_key = current_node["key"]
+                if key > current_key: # уходим в правое полудерево
+                    right_node = current_node["right"]:
+                    if right_node is None:
+                        current_node["right"] = self._create_node(key, value)
+                        break
+                    current_node = current_node["right"]
+                else:
+                    left_node = current_node["left"]
+                    if left_node is None:
+                        current_node["left"] = self._create_node(key, value)
+
+
+        return None
+
         """
         Insert (key, value) pair to binary search tree
 
