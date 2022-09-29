fdba18922279166ca350e59727d283a0d1d0a740
russos <ro0ne@mail.ru>
2022-09-29 18:41:04 +0300

    работает

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -10,15 +10,20 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    numbers = 51# терминальное значение
-    container = [] #пустой список
-    for i in range(numbers): #итерация в диапазоне 31
-        container.append(randint(-101, 101)) #добавление в пустой список чисел от минус ста до ста
-
-    for i in range(numbers-1):
-        for j in range(numbers-i-1):
-            if container[j] > container[j + 1]:
-                container[j], container[j + 1] = container[j + 1], container[j]
-
-    #return container
-                print(container)
+    # numbers = 51# терминальное значение
+    # container = [] #пустой список
+    # for i in range(numbers): #итерация в диапазоне 31
+    #     container.append(randint(-101, 101)) #добавление в пустой список чисел от минус ста до ста
+    #
+    # for i in range(numbers-1):
+    #     for j in range(numbers-i-1):
+    #         if container[j] > container[j + 1]:
+    #             container[j], container[j + 1] = container[j + 1], container[j]
+    #
+    # #return container
+    #             print(container)
+    for _ in range(len(container)):
+        for i in range(len(container)-1):
+            if container[i] > container[i+1]:
+                container[i],container[i+1] = container[i +1],container[i]
+    return container
