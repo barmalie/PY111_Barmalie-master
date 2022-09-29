c6ffbe949041c0e14e4df7e9e3e50fd8beed887b
russos <ro0ne@mail.ru>
2022-09-29 18:07:29 +0300

    commit

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -4,6 +4,7 @@ from typing import List
 def sort(container: List[int]) -> List[int]:
     """
     Sort input container with merge sort
+    Сортировка миграцией
 
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
