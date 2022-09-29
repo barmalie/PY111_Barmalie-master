1c52718d84cc71007b0ef35704d979d009b1479f
russos <ro0ne@mail.ru>
2022-09-29 20:27:10 +0300

    разбираемся

--- Tasks/g1_merge_sort.py
+++ Tasks/g1_merge_sort.py
@@ -47,28 +47,66 @@ from operator import lt, gt
 #     print(min(list_))
 #     print(reversed(sort(list_)))
 
-def merge(left, right):
-    right_index, left_index = 0, 0
-    list = []
-    while len(right_index) < len(right) and len(left_index) < len(right):
-        if left_index(left) < right_index(right):
-            list.append(right_index)
-            left_index += 1
-        else:
-            list.append(right[right_index])
-            right_index += 1
+# def merge(left, right):
+#     right_index, left_index = 0, 0
+#     list = []
+#     while len(right_index) < len(right) and len(left_index) < len(right):
+#         if left_index(left) < right_index(right):
+#             list.append(right_index)
+#             left_index += 1
+#         else:
+#             list.append(right[right_index])
+#             right_index += 1
+#
+#     list += left[left_index:]
+#     list += right[right_index:]
+#     return list
+#
+#
+#
+# def merge_sort(array):
+#     if len(array) <= 1:# базовый случай
+#         return array
+#     half = len(array) // 2
+#     left = merge_sort(array[:half])
+#     right = merge_sort(array[half:])
+#
+#     return merge_sort(left, right)
 
-    list += left[left_index:]
-    list += right[right_index:]
-    return list
+def merge_function(sorted_left, sorted_right):
+    sorted_result = []
+    current_left_index = 0
+    current_right_index = 0
+    while True:
+        current_left_value = sorted_left(current_left_index)
+        current_right_value = sorted_right(current_right_index)
+        if current_left_value < current_right_value:
+            # берем значение левой части
+            # в итоговый массив записываем наименьшее
+            sorted_result.append(current_left_value)
+            current_left_index += 1
+        else: # берем значение из правой части
+            sorted_result.append(current_right_value)
+            current_right_index += 1
+            # прописываем алгоритм для заброски в массив последнего числа
+        if current_right_index == len(sorted_left):
+            sorted_result.extend(sorted_right[current_right_index:])
+            break
 
+        elif current_right_index == len(sorted_right):
+            sorted_result.extend(sorted_left[current_right_index:])
+            # sorted_result = sorted_result + sorted_right
+            break
 
+    return  sorted_result
 
-def merge_sort(array):
-    if len(array) <= 1:# базовый случай
-        return array
-    half = len(array) // 2
-    left = merge_sort(array[:half])
-    right = merge_sort(array[half:])
-
-    return merge_sort(left, right)
+def sort(container):
+    # терминальный случай
+    if len(container) == 1:
+        return container
+    middle_index = len(container)//2
+    left_part = sort(container[:middle_index])
+    right_part = sort(container[middle_index:])
+    # обход в глубину
+    # в данной функции сделали разбиение
+    return merge_func(left_part, right_part)
